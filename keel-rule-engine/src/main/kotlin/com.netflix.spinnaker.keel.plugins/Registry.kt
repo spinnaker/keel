31b32c3cccd280732673ee3376200c48f4275930
Rob Fletcher <rfletcher@netflix.com>
2018-08-06 16:55:26 -0700

    Added veto plugin registry and common-ized some code

--- keel-rule-engine/src/main/kotlin/com.netflix.spinnaker.keel.plugins/Registry.kt
+++ keel-rule-engine/src/main/kotlin/com.netflix.spinnaker.keel.plugins/Registry.kt
@@ -1,49 +1,20 @@
 package com.netflix.spinnaker.keel.plugins
 
 import com.netflix.discovery.EurekaClient
-import com.netflix.spinnaker.keel.api.TypeMetadata
-import com.netflix.spinnaker.keel.api.engine.RegisterRequest
-import com.netflix.spinnaker.keel.api.engine.RegisterResponse
-import com.netflix.spinnaker.keel.api.engine.RegistryGrpc.RegistryImplBase
-import com.netflix.spinnaker.keel.api.plugin.AssetPluginGrpc
+import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
-import io.grpc.stub.StreamObserver
-import org.slf4j.LoggerFactory
+import io.grpc.stub.AbstractStub
 
-class Registry(
-  private val eurekaClient: EurekaClient
-) : RegistryImplBase() {
+interface Registry<T : AbstractStub<T>> {
 
-  private val log = LoggerFactory.getLogger(javaClass)
-  private val assetPlugins: MutableMap<TypeMetadata, String> = mutableMapOf()
+  val eurekaClient: EurekaClient
+  val stubFactory: (ManagedChannel) -> T
 
-  fun pluginFor(type: TypeMetadata): AssetPluginGrpc.AssetPluginBlockingStub =
-    assetPlugins[type]?.let { name ->
-      val address = eurekaClient.getNextServerFromEureka(name, false)
-      ManagedChannelBuilder
-        .forAddress(address.ipAddr, address.port)
-        .usePlaintext()
-        .build()
-        .let { channel ->
-          AssetPluginGrpc.newBlockingStub(channel)
-        }
-    } ?: throw UnsupportedAssetType(type)
-
-  override fun registerAssetPlugin(request: RegisterRequest, responseObserver: StreamObserver<RegisterResponse>) {
-    request
-      .typesList
-      .forEach { type ->
-        assetPlugins[type] = request.name
-        log.info("Registered asset plugin \"${request.name}\" supporting $type")
-      }
-    responseObserver.apply {
-      onNext(
-        RegisterResponse
-          .newBuilder()
-          .apply { succeeded = true }
-          .build()
-      )
-      onCompleted()
-    }
+  fun stubFor(name: String): T {
+    val address = eurekaClient.getNextServerFromEureka(name, false)
+    return ManagedChannelBuilder.forAddress(address.ipAddr, address.port)
+      .usePlaintext()
+      .build()
+      .let(stubFactory)
   }
 }
