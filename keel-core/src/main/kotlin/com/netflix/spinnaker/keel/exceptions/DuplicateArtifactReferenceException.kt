d5b9f2c99cc4d4ed09dd0e39498c75c73e78414c
Lorin Hochstein <lorinh@gmail.com>
2020-03-02 15:46:55 -0800

    feat(repository): simplify error message
    Co-Authored-By: Luis Fernando Pollo <1323478+luispollo@users.noreply.github.com>

--- keel-core/src/main/kotlin/com/netflix/spinnaker/keel/exceptions/DuplicateArtifactReferenceException.kt
+++ keel-core/src/main/kotlin/com/netflix/spinnaker/keel/exceptions/DuplicateArtifactReferenceException.kt
@@ -3,6 +3,6 @@ package com.netflix.spinnaker.keel.exceptions
 class DuplicateArtifactReferenceException(
   private val artifactNameToRef: Map<String, String>
 ) : ValidationException(
-  "Multiple artifacts are using the same string as a reference: $artifactNameToRef. " +
+  "Multiple artifacts are using the same reference: $artifactNameToRef. " +
     "Please ensure each artifact has a unique reference."
 )
