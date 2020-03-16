90ed996538bb786621da1e407e59f7c5f8ce74dd
Luis Pollo <lpollo@netflix.com>
2020-03-16 09:23:48 -0700

    Replace JSON with YAML in tests

--- keel-web/src/test/kotlin/com/netflix/spinnaker/keel/rest/ApplicationControllerTests.kt
+++ keel-web/src/test/kotlin/com/netflix/spinnaker/keel/rest/ApplicationControllerTests.kt
@@ -11,6 +11,7 @@ import com.netflix.spinnaker.keel.api.artifacts.ArtifactStatus
 import com.netflix.spinnaker.keel.api.artifacts.DebianArtifact
 import com.netflix.spinnaker.keel.api.ec2.ArtifactImageProvider
 import com.netflix.spinnaker.keel.api.ec2.ClusterSpec
+import com.netflix.spinnaker.keel.diff.DefaultResourceDiff
 import com.netflix.spinnaker.keel.ec2.SPINNAKER_EC2_API_V1
 import com.netflix.spinnaker.keel.events.ResourceCreated
 import com.netflix.spinnaker.keel.pause.ActuationPauser
@@ -18,11 +19,14 @@ import com.netflix.spinnaker.keel.persistence.memory.InMemoryArtifactRepository
 import com.netflix.spinnaker.keel.persistence.memory.InMemoryDeliveryConfigRepository
 import com.netflix.spinnaker.keel.persistence.memory.InMemoryResourceRepository
 import com.netflix.spinnaker.keel.serialization.configuredObjectMapper
+import com.netflix.spinnaker.keel.serialization.configuredYamlMapper
 import com.netflix.spinnaker.keel.spring.test.MockEurekaConfiguration
 import com.netflix.spinnaker.keel.test.resource
+import com.netflix.spinnaker.keel.yaml.APPLICATION_YAML_VALUE
 import dev.minutest.junit.JUnit5Minutests
 import dev.minutest.rootContext
 import io.mockk.clearAllMocks
+import java.nio.charset.StandardCharsets
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -31,7 +35,10 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MvcResult
+import org.springframework.test.web.servlet.ResultMatcher
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.ContentResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import strikt.api.expectThat
@@ -58,6 +65,10 @@ internal class ApplicationControllerTests : JUnit5Minutests {
 
   @Autowired
   lateinit var actuationPauser: ActuationPauser
+
+  private val jsonMapper = configuredObjectMapper()
+
+  private val yamlMapper = configuredYamlMapper()
 
   object Fixture {
     const val application = "fnord"
@@ -102,8 +113,6 @@ internal class ApplicationControllerTests : JUnit5Minutests {
       artifacts = setOf(artifact),
       environments = environments.values.toSet()
     )
-
-    val jsonMapper = configuredObjectMapper()
   }
 
   fun tests() = rootContext<Fixture> {
@@ -185,229 +194,170 @@ internal class ApplicationControllerTests : JUnit5Minutests {
 
       test("can get resource summaries by application") {
         val request = get("/application/$application?entities=resources")
-          .accept(MediaType.APPLICATION_JSON_VALUE)
+          .accept(APPLICATION_YAML_VALUE)
         mvc
           .perform(request)
           .andExpect(status().isOk)
           .andDo { println(it.response.contentAsString) }
-          .andExpect(content().json(
+          .andExpect(content().yaml(
             """
-            {
-              "applicationPaused": false,
-              "hasManagedResources": true,
-              "currentEnvironmentConstraints": [],
-              "resources": [
-                {
-                  "id": "ec2:cluster:test:fnord-test",
-                  "kind": "ec2/cluster@v1",
-                  "status": "CREATED"
-                },
-                {
-                  "id": "ec2:cluster:test:fnord-staging",
-                  "kind": "ec2/cluster@v1",
-                  "status": "CREATED"
-                },
-                {
-                  "id": "ec2:cluster:test:fnord-production",
-                  "kind": "ec2/cluster@v1",
-                  "status": "CREATED"
-                }
-              ]
-            }
+            applicationPaused: false
+            hasManagedResources: true
+            currentEnvironmentConstraints: []
+            resources:
+            - id: "ec2:cluster:test:fnord-test"
+              kind: "ec2/cluster@v1"
+              status: "CREATED"
+              moniker:
+                app: "fnord"
+                stack: "test"
+              locations:
+                account: "test"
+                vpc: "vpc0"
+                regions:
+                - name: "us-west-2"
+            - id: "ec2:cluster:test:fnord-staging"
+              kind: "ec2/cluster@v1"
+              status: "CREATED"
+              moniker:
+                app: "fnord"
+                stack: "staging"
+              locations:
+                account: "test"
+                vpc: "vpc0"
+                regions:
+                - name: "us-west-2"
+            - id: "ec2:cluster:test:fnord-production"
+              kind: "ec2/cluster@v1"
+              status: "CREATED"
+              moniker:
+                app: "fnord"
+                stack: "production"
+              locations:
+                account: "test"
+                vpc: "vpc0"
+                regions:
+                - name: "us-west-2"
             """.trimIndent()
           ))
       }
 
       test("can get environment summaries by application") {
         val request = get("/application/$application?entities=environments")
-          .accept(MediaType.APPLICATION_JSON_VALUE)
+          .accept(APPLICATION_YAML_VALUE)
         mvc
           .perform(request)
           .andExpect(status().isOk)
           .andDo { println(it.response.contentAsString) }
-          .andExpect(content().json(
+          .andExpect(content().yaml(
             """
-            {
-              "applicationPaused": false,
-              "hasManagedResources": true,
-              "currentEnvironmentConstraints": [],
-              "environments": [
-                {
-                  "name": "test",
-                  "artifacts": [
-                    {
-                      "name": "fnord",
-                      "type": "deb",
-                      "statuses": [
-                        "RELEASE"
-                      ],
-                      "versions": {
-                        "current": "1.0.2",
-                        "pending": [],
-                        "approved": [
-                          "1.0.3"
-                        ],
-                        "previous": [
-                          "1.0.0",
-                          "1.0.1"
-                        ],
-                        "vetoed": []
-                      }
-                    }
-                  ],
-                  "resources": [
-                    "ec2:cluster:test:fnord-test"
-                  ]
-                },
-                {
-                  "name": "staging",
-                  "artifacts": [
-                    {
-                      "name": "fnord",
-                      "type": "deb",
-                      "statuses": [
-                        "RELEASE"
-                      ],
-                      "versions": {
-                        "current": "1.0.1",
-                        "pending": [
-                          "1.0.2",
-                          "1.0.3"
-                        ],
-                        "approved": [],
-                        "previous": [
-                          "1.0.0"
-                        ],
-                        "vetoed": []
-                      }
-                    }
-                  ],
-                  "resources": [
-                    "ec2:cluster:test:fnord-staging"
-                  ]
-                },
-                {
-                  "name": "production",
-                  "artifacts": [
-                    {
-                      "name": "fnord",
-                      "type": "deb",
-                      "statuses": [
-                        "RELEASE"
-                      ],
-                      "versions": {
-                        "current": "1.0.0",
-                        "pending": [
-                          "1.0.1",
-                          "1.0.2",
-                          "1.0.3"
-                        ],
-                        "approved": [],
-                        "previous": [],
-                        "vetoed": []
-                      }
-                    }
-                  ],
-                  "resources": [
-                    "ec2:cluster:test:fnord-production"
-                  ]
-                }
-              ]
-            }
-            """.trimMargin()
+            applicationPaused: false
+            hasManagedResources: true
+            currentEnvironmentConstraints: []
+            environments:
+            - artifacts:
+              - name: "fnord"
+                type: "deb"
+                statuses:
+                - "RELEASE"
+                versions:
+                  current: "1.0.2"
+                  pending: []
+                  approved:
+                  - "1.0.3"
+                  previous:
+                  - "1.0.0"
+                  - "1.0.1"
+                  vetoed: []
+              name: "test"
+              resources:
+              - "ec2:cluster:test:fnord-test"
+            - artifacts:
+              - name: "fnord"
+                type: "deb"
+                statuses:
+                - "RELEASE"
+                versions:
+                  current: "1.0.1"
+                  pending:
+                  - "1.0.2"
+                  - "1.0.3"
+                  approved: []
+                  previous:
+                  - "1.0.0"
+                  vetoed: []
+              name: "staging"
+              resources:
+              - "ec2:cluster:test:fnord-staging"
+            - artifacts:
+              - name: "fnord"
+                type: "deb"
+                statuses:
+                - "RELEASE"
+                versions:
+                  current: "1.0.0"
+                  pending:
+                  - "1.0.1"
+                  - "1.0.2"
+                  - "1.0.3"
+                  approved: []
+                  previous: []
+                  vetoed: []
+              name: "production"
+              resources:
+              - "ec2:cluster:test:fnord-production"
+            """.trimIndent()
           ))
       }
 
       test("can get artifact summaries by application") {
         val request = get("/application/$application?entities=artifacts")
-          .accept(MediaType.APPLICATION_JSON_VALUE)
+          .accept(APPLICATION_YAML_VALUE)
         mvc
           .perform(request)
           .andExpect(status().isOk)
           .andDo { println(it.response.contentAsString) }
-          .andExpect(content().json(
+          .andExpect(content().yaml(
             """
-            {
-              "applicationPaused": false,
-              "hasManagedResources": true,
-              "currentEnvironmentConstraints": [],
-              "artifacts": [
-                {
-                  "name": "fnord",
-                  "type": "deb",
-                  "versions": [
-                    {
-                      "version": "1.0.0",
-                      "environments": [
-                        {
-                          "name": "test",
-                          "state": "previous"
-                        },
-                        {
-                          "name": "staging",
-                          "state": "previous"
-                        },
-                        {
-                          "name": "production",
-                          "state": "current"
-                        }
-                      ]
-                    },
-                    {
-                      "version": "1.0.1",
-                      "environments": [
-                        {
-                          "name": "test",
-                          "state": "previous"
-                        },
-                        {
-                          "name": "staging",
-                          "state": "current"
-                        },
-                        {
-                          "name": "production",
-                          "state": "pending"
-                        }
-                      ]
-                    },
-                    {
-                      "version": "1.0.2",
-                      "environments": [
-                        {
-                          "name": "test",
-                          "state": "current"
-                        },
-                        {
-                          "name": "staging",
-                          "state": "pending"
-                        },
-                        {
-                          "name": "production",
-                          "state": "pending"
-                        }
-                      ]
-                    },
-                    {
-                      "version": "1.0.3",
-                      "environments": [
-                        {
-                          "name": "test",
-                          "state": "approved"
-                        },
-                        {
-                          "name": "staging",
-                          "state": "pending"
-                        },
-                        {
-                          "name": "production",
-                          "state": "pending"
-                        }
-                      ]
-                    }
-                  ]
-                }
-              ]
-            }
+            applicationPaused: false
+            hasManagedResources: true
+            currentEnvironmentConstraints: []
+            artifacts:
+            - name: "fnord"
+              type: "deb"
+              versions:
+              - version: "1.0.0"
+                environments:
+                - name: "test"
+                  state: "previous"
+                - name: "staging"
+                  state: "previous"
+                - name: "production"
+                  state: "current"
+              - version: "1.0.1"
+                environments:
+                - name: "test"
+                  state: "previous"
+                - name: "staging"
+                  state: "current"
+                - name: "production"
+                  state: "pending"
+              - version: "1.0.2"
+                environments:
+                - name: "test"
+                  state: "current"
+                - name: "staging"
+                  state: "pending"
+                - name: "production"
+                  state: "pending"
+              - version: "1.0.3"
+                environments:
+                - name: "test"
+                  state: "approved"
+                - name: "staging"
+                  state: "pending"
+                - name: "production"
+                  state: "pending"
             """.trimIndent()
           ))
       }
@@ -490,6 +440,21 @@ internal class ApplicationControllerTests : JUnit5Minutests {
             "currentEnvironmentConstraints",
             "resources"
           )
+      }
+    }
+  }
+
+  private fun ContentResultMatchers.yaml(expectedYaml: String): ResultMatcher {
+    return ResultMatcher { result: MvcResult ->
+      val content = result.response.getContentAsString(StandardCharsets.UTF_8)
+      val actual = yamlMapper.readValue<Map<String, Any?>>(content)
+      val expected = yamlMapper.readValue<Map<String, Any?>>(expectedYaml)
+      val diff = DefaultResourceDiff(actual, expected)
+
+      if (!diff.hasChanges()) {
+        true
+      } else {
+        throw AssertionError("Actual and expected YAML differ: \n${diff.toDeltaJson()}")
       }
     }
   }
