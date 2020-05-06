ce5ca8998dc5a0cdd94dbd8527166a6ebdfc093d
Luis Pollo <lpollo@netflix.com>
2020-05-05 19:32:00 -0700

    fix(pr): Remove redundant ResourceActuationPaused synthetic event

--- keel-core/src/main/kotlin/com/netflix/spinnaker/keel/events/ResourceHistoryListener.kt
+++ keel-core/src/main/kotlin/com/netflix/spinnaker/keel/events/ResourceHistoryListener.kt
@@ -1,30 +1,16 @@
 package com.netflix.spinnaker.keel.events
 
-import com.netflix.spinnaker.keel.api.serviceAccount
-import com.netflix.spinnaker.keel.pause.ActuationPauser
 import com.netflix.spinnaker.keel.persistence.ResourceRepository
-import java.time.Clock
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
 class ResourceHistoryListener(
-  private val resourceRepository: ResourceRepository,
-  private val actuationPauser: ActuationPauser,
-  private val clock: Clock
+  private val resourceRepository: ResourceRepository
 ) {
 
   @EventListener(ResourceEvent::class)
   fun onResourceEvent(event: ResourceEvent) {
     resourceRepository.appendHistory(event)
-
-    // Account for the case where a resource was paused, then deleted (i.e. removed from management), then
-    // resubmitted, where we don't want to inadvertently resume actuation without the user knowing and giving
-    // explicit consent, by injecting a ResourceActuationPaused event.
-    // ApplicationActuationPaused events are injected in the resource event history dynamically.
-    if (event is ResourceCreated && actuationPauser.resourceIsPaused(event.id)) {
-      val resource = resourceRepository.get(event.id)
-      resourceRepository.appendHistory(ResourceActuationPaused(resource, resource.serviceAccount, clock))
-    }
   }
 }
--- keel-core/src/test/kotlin/com/netflix/spinnaker/keel/events/ResourceHistoryListenerTests.kt
+++ keel-core/src/test/kotlin/com/netflix/spinnaker/keel/events/ResourceHistoryListenerTests.kt
@@ -1,7 +1,5 @@
 package com.netflix.spinnaker.keel.events
 
-import com.netflix.spinnaker.keel.api.id
-import com.netflix.spinnaker.keel.pause.ActuationPauser
 import com.netflix.spinnaker.keel.persistence.ResourceRepository
 import com.netflix.spinnaker.keel.test.resource
 import dev.minutest.junit.JUnit5Minutests
@@ -11,17 +9,13 @@ import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
-import java.time.Clock
 
 class ResourceHistoryListenerTests : JUnit5Minutests {
   object Fixture {
-    val clock = Clock.systemUTC()
     val resourceRepository: ResourceRepository = mockk()
-    val actuationPauser: ActuationPauser = mockk()
-    val listener = ResourceHistoryListener(resourceRepository, actuationPauser, clock)
+    val listener = ResourceHistoryListener(resourceRepository)
     val resource = resource()
     val resourceValidEvent = ResourceValid(resource)
-    val resourceCreatedEvent = ResourceCreated(resource)
   }
 
   fun tests() = rootContext<Fixture> {
@@ -41,31 +35,6 @@ class ResourceHistoryListenerTests : JUnit5Minutests {
       test("event is persisted") {
         verify(exactly = 1) {
           resourceRepository.appendHistory(resourceValidEvent)
-        }
-      }
-    }
-
-    context("resource created event received and resource is paused") {
-      before {
-        every {
-          resourceRepository.appendHistory(any() as ResourceEvent)
-        } just Runs
-
-        every {
-          actuationPauser.resourceIsPaused(resource.id)
-        } returns true
-
-        every {
-          resourceRepository.get(resource.id)
-        } returns resource
-
-        listener.onResourceEvent(resourceCreatedEvent)
-      }
-
-      test("resource created and paused events are persisted") {
-        verify(exactly = 1) {
-          resourceRepository.appendHistory(resourceCreatedEvent)
-          resourceRepository.appendHistory(ofType(ResourceActuationPaused::class))
         }
       }
     }
