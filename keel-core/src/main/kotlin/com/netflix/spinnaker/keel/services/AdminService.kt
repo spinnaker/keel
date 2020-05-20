6e2e201759956b7394d20184f074fa4a6ec520a8
Emily Burns <emjeburns@gmail.com>
2020-05-20 15:27:26 -0700

    feat(admin): redo constraint evaluation

--- keel-core/src/main/kotlin/com/netflix/spinnaker/keel/services/AdminService.kt
+++ keel-core/src/main/kotlin/com/netflix/spinnaker/keel/services/AdminService.kt
@@ -1,9 +1,11 @@
 package com.netflix.spinnaker.keel.services
 
+import com.netflix.spinnaker.keel.api.StatefulConstraint
 import com.netflix.spinnaker.keel.core.api.ApplicationSummary
 import com.netflix.spinnaker.keel.pause.ActuationPauser
 import com.netflix.spinnaker.keel.persistence.DiffFingerprintRepository
 import com.netflix.spinnaker.keel.persistence.KeelRepository
+import com.netflix.spinnaker.kork.exceptions.UserException
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
@@ -31,5 +33,28 @@ class AdminService(
   fun triggerRecheck(resourceId: String) {
     log.info("Triggering a recheck for $resourceId by clearing our record of the diff")
     diffFingerprintRepository.clear(resourceId)
+  }
+
+  /**
+   * Removes the stored state we have for any stateful constraints for an environment
+   * so they will evaluate again
+   */
+  fun redoConstraints(application: String, environment: String, type: String? = null) {
+    log.info("[app=$application, env=$environment] Triggering a redo of stateful constraints.")
+    if (type != null) {
+      log.info("[app=$application, env=$environment] Triggering only type $type")
+    }
+
+    val deliveryConfig = repository.getDeliveryConfigForApplication(application)
+    val env = deliveryConfig.environments.find { it.name == environment }
+      ?: throw UserException("No environment $environment in config for $application")
+    env.constraints.forEach { constraint ->
+      if (constraint is StatefulConstraint) {
+        if (type == null || type == constraint.type) {
+          log.info("[app=$application, env=$environment] Deleting constraint state for ${constraint.type}.")
+          repository.deleteConstraintState(deliveryConfig.name, environment, constraint.type)
+        }
+      }
+    }
   }
 }
