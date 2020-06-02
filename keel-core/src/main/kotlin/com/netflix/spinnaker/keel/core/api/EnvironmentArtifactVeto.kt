82f2996c4594341a7bbdaf25c42f61ada4474c3e
Emily Burns <emjeburns@gmail.com>
2020-06-02 12:17:54 -0700

    feat(mark-as-bad): add comment, show comment

--- keel-core/src/main/kotlin/com/netflix/spinnaker/keel/core/api/EnvironmentArtifactVeto.kt
+++ keel-core/src/main/kotlin/com/netflix/spinnaker/keel/core/api/EnvironmentArtifactVeto.kt
@@ -3,20 +3,27 @@ package com.netflix.spinnaker.keel.core.api
 import com.netflix.spinnaker.keel.api.artifacts.DeliveryArtifact
 import java.time.Instant
 
-data class EnvironmentArtifactPin(
+data class EnvironmentArtifactVeto(
   val targetEnvironment: String,
   val reference: String,
   val version: String,
-  val pinnedBy: String?,
+  val vetoedBy: String?,
   val comment: String?
 )
 
-data class PinnedEnvironment(
+data class VetoedArtifact(
   val deliveryConfigName: String,
   val targetEnvironment: String,
   val artifact: DeliveryArtifact,
   val version: String,
-  val pinnedBy: String?,
-  val pinnedAt: Instant?,
+  val vetoedBy: String?,
+  val vetoedAt: Instant?,
   val comment: String?
+)
+
+data class EnvironmentArtifactVetoes(
+  val deliveryConfigName: String,
+  val targetEnvironment: String,
+  val artifact: DeliveryArtifact,
+  val versions: MutableSet<String>
 )
