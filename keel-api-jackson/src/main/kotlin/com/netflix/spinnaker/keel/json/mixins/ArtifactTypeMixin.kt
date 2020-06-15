746b1216b658db16e99d3e004089825e1d286b70
Luis Pollo <lpollo@netflix.com>
2020-06-14 19:32:31 -0700

    fix(pr): Remove ArtifactTypeMixin

--- keel-api-jackson/src/main/kotlin/com/netflix/spinnaker/keel/json/mixins/ArtifactTypeMixin.kt
+++
@@ -1,8 +0,0 @@
-package com.netflix.spinnaker.keel.json.mixins
-
-import com.fasterxml.jackson.annotation.JsonValue
-
-internal interface ArtifactTypeMixin {
-  @get:JsonValue
-  val name: String
-}
