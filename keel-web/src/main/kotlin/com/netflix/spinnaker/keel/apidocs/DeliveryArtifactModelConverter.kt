b01e01b0e9c3d2b57e5dadff56ba277fb1171239
Luis Pollo <lpollo@netflix.com>
2020-06-14 20:22:12 -0700

    refactor(artifacts): Register DeliveryArtifact sub-types automatically for API docs

--- keel-web/src/main/kotlin/com/netflix/spinnaker/keel/apidocs/DeliveryArtifactModelConverter.kt
+++ keel-web/src/main/kotlin/com/netflix/spinnaker/keel/apidocs/DeliveryArtifactModelConverter.kt
@@ -1,24 +1,23 @@
 package com.netflix.spinnaker.keel.apidocs
 
 import com.netflix.spinnaker.keel.api.artifacts.DeliveryArtifact
-import com.netflix.spinnaker.keel.artifact.DEB
-import com.netflix.spinnaker.keel.artifact.DOCKER
-import com.netflix.spinnaker.keel.artifact.DebianArtifact
-import com.netflix.spinnaker.keel.artifact.DockerArtifact
+import com.netflix.spinnaker.keel.api.plugins.ArtifactPublisher
 import org.springframework.stereotype.Component
 
 @Component
-class DeliveryArtifactModelConverter : SubtypesModelConverter<DeliveryArtifact>(DeliveryArtifact::class.java) {
-  override val subTypes = listOf(
-    DebianArtifact::class.java,
-    DockerArtifact::class.java
-  )
+class DeliveryArtifactModelConverter(
+  artifactPublishers: List<ArtifactPublisher<*>>
+) : SubtypesModelConverter<DeliveryArtifact>(DeliveryArtifact::class.java) {
 
   override val discriminator: String? = DeliveryArtifact::type.name
 
-  // TODO: can we just work this out automatically?
-  override val mapping: Map<String, Class<out DeliveryArtifact>> = mapOf(
-    DEB to DebianArtifact::class.java,
-    DOCKER to DockerArtifact::class.java
-  )
+  override val subTypes: List<Class<out DeliveryArtifact>> =
+    artifactPublishers.map {
+      it.supportedArtifact.artifactClass
+    }
+
+  override val mapping: Map<String, Class<out DeliveryArtifact>> =
+    artifactPublishers.map {
+      it.supportedArtifact.name to it.supportedArtifact.artifactClass
+    }.toMap()
 }
