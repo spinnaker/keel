91aace0e26f6f39c9689786535dd716073a871fe
Rob Fletcher <robfletcher@users.noreply.github.com>
2020-07-02 13:22:09 -0700

    fix(kdoc): fixed typo
    Co-authored-by: Luis Pollo <1323478+luispollo@users.noreply.github.com>

--- keel-api/src/main/kotlin/com/netflix/spinnaker/keel/api/ExcludedFromDiff.kt
+++ keel-api/src/main/kotlin/com/netflix/spinnaker/keel/api/ExcludedFromDiff.kt
@@ -3,7 +3,7 @@ package com.netflix.spinnaker.keel.api
 import kotlin.annotation.AnnotationTarget.PROPERTY_GETTER
 
 /**
- * Indicates that differences between current and desired versions of a property should not be
+ * Indicates that differences between current and desired versions of a property should be
  * ignored. This may be because the property is not relevant to the state but needs to be included
  * in the model for other reasons (for example a server group's name), or because modifying the
  * property after a resource is created is not possible and therefore Keel should avoid flapping
