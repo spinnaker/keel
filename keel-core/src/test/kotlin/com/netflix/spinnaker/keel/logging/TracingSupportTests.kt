971a4b928cf33e6d6350ccfe1162612fa1723052
Luis Pollo <lpollo@netflix.com>
2020-09-15 16:38:56 -0700

    test(tracing): One more (unsuccessul) try at reproducing mixed X-SPINNAKER-RESOURCE-ID

--- keel-core/src/test/kotlin/com/netflix/spinnaker/keel/logging/TracingSupportTests.kt
+++ keel-core/src/test/kotlin/com/netflix/spinnaker/keel/logging/TracingSupportTests.kt
@@ -7,17 +7,24 @@ import com.netflix.spinnaker.keel.logging.TracingSupport.Companion.withTracingCo
 import com.netflix.spinnaker.keel.test.resource
 import dev.minutest.junit.JUnit5Minutests
 import dev.minutest.rootContext
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.supervisorScope
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
 import org.slf4j.MDC
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 import strikt.assertions.isNull
 import java.lang.Thread.sleep
+import java.time.Instant
+import kotlin.coroutines.CoroutineContext
 import kotlin.random.Random
 
 class TracingSupportTests : JUnit5Minutests {
@@ -93,7 +100,6 @@ class TracingSupportTests : JUnit5Minutests {
       }
 
       test("does not mix up X-SPINNAKER-RESOURCE-ID between parallel coroutines") {
-        val anotherResource = resource()
         val resources = (1..1000).associate { "resource-$it" to resource(id="resource-$it") }
         val coroutines = resources.map { (id, resource) ->
           GlobalScope.launch {
@@ -108,6 +114,59 @@ class TracingSupportTests : JUnit5Minutests {
         runBlocking {
           coroutines.joinAll()
         }
+      }
+
+      test("does not mix up X-SPINNAKER-RESOURCE-ID in simulated actuation path") {
+        val TestScope = object : CoroutineScope {
+          override val coroutineContext: CoroutineContext = Dispatchers.IO
+
+          fun run() {
+            // --- start CheckScheduler.checkResources coroutines scopes
+            val job = launch {
+              supervisorScope {
+                val resources = (1..200).map { resource(id = "resource-${Random.nextInt(1, 200)}") }
+                resources.forEach { resource ->
+                  try {
+                    withTimeout(45) {
+                      launch {
+                        // --- start ResourceActuator.checkResource coroutine scopes
+                        withTracingContext(resource) {
+                          try {
+                            val delay = Random.nextLong(0, 50)
+                            if (delay > 45) {
+                              throw RuntimeException("Oops!")
+                            }
+                            sleep(delay)
+                            println("[${Thread.currentThread().name}] X-SPINNAKER-RESOURCE-ID: ${MDC.get(X_SPINNAKER_RESOURCE_ID)}")
+                            expectThat(MDC.get(X_SPINNAKER_RESOURCE_ID))
+                              .isEqualTo(resource.id)
+                          } catch (e: Exception) {
+                            println(e.message)
+                          }
+                        }
+                      }
+                    }
+                  } catch (e: TimeoutCancellationException) {
+                    println("Timed out!")
+                  }
+                }
+              }
+            }
+            runBlocking { job.join() }
+          }
+        }
+
+        val scheduler = ThreadPoolTaskScheduler()
+        scheduler.threadNamePrefix = "scheduler-"
+        scheduler.poolSize = 5
+
+        scheduler.initialize()
+        scheduler.schedule({ TestScope.run() }, Instant.now())
+        // start a second task to simulate concurrent executions, just in case that's what's going on...
+        scheduler.schedule({ TestScope.run() }, Instant.now())
+
+        sleep(45 * 200)
+        println("Done waiting...")
       }
     }
   }
