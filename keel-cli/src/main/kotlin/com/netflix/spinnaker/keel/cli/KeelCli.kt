9d9fa784caa7bb16c18855fee56275f63bcf0741
Luis Pollo <lpollo@netflix.com>
2020-09-29 21:23:00 -0700

    feat(cli): Keel data generation CLI

---
+++ keel-cli/src/main/kotlin/com/netflix/spinnaker/keel/cli/KeelCli.kt
@@ -0,0 +1,68 @@
+package com.netflix.spinnaker.keel.cli
+
+import com.netflix.spinnaker.keel.cli.commands.DataGenCommand
+import kotlinx.cli.ArgParser
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.WebApplicationType.NONE
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
+import java.lang.annotation.ElementType.TYPE
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy.RUNTIME
+import java.lang.annotation.Target
+
+
+private val DEFAULT_PROPS = mapOf(
+  "netflix.environment" to "test",
+  "netflix.account" to "\${netflix.environment}",
+  "netflix.stack" to "test",
+  "spring.config.additional-location" to "\${user.home}/.spinnaker/",
+  "spring.application.name" to "keel-cli",
+  "spring.config.name" to "spinnaker,keel",
+  "spring.profiles.active" to "\${netflix.environment},local,cli",
+  // TODO: not sure why we need this when it should get loaded from application.properties
+  "spring.main.allow-bean-definition-overriding" to "true",
+  "spring.groovy.template.check-template-location" to "false"
+)
+
+/**
+ * A simple Spring Boot console application providing CLI access to Keel.
+ */
+@KeelCliComponentScan
+@SpringBootApplication
+class KeelCli(
+  val dataGenCommand: DataGenCommand
+) : CommandLineRunner {
+  override fun run(args: Array<String>) {
+    val parser = ArgParser("keel-cli")
+    parser.subcommands(dataGenCommand)
+    parser.parse(args)
+  }
+}
+
+@Retention(RUNTIME)
+@Target(TYPE)
+@ComponentScan(
+  basePackages = [
+    "com.netflix.spinnaker.config",
+    "com.netflix.spinnaker.keel"
+  ],
+  excludeFilters = [ComponentScan.Filter(
+    type = FilterType.REGEX,
+    pattern = [
+      "com.netflix.spinnaker.config.DefaultServiceClientProvider",
+      "com.netflix.spinnaker.config.MetricsEndpointConfiguration",
+      "com.netflix.spinnaker.keel.artifacts.ArtifactListener"
+    ]
+  )]
+)
+annotation class KeelCliComponentScan
+
+fun main(args: Array<String>) {
+  SpringApplicationBuilder(KeelCli::class.java)
+    .properties(DEFAULT_PROPS)
+    .web(NONE)
+    .run(*args)
+}
