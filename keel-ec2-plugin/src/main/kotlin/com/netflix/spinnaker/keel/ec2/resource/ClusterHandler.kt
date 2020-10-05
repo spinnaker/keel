403739fbba6242448fbc4573cde6c2733ec8b2a2
Lorin Hochstein <lorinh@gmail.com>
2020-10-05 09:25:57 -0700

    feature(ec2): Add support for launch templates
    Add support for handling ec2-based apps that have been configured in clouddriver to use the
    AWS launch templates.

--- keel-ec2-plugin/src/main/kotlin/com/netflix/spinnaker/keel/ec2/resource/ClusterHandler.kt
+++ keel-ec2-plugin/src/main/kotlin/com/netflix/spinnaker/keel/ec2/resource/ClusterHandler.kt
@@ -944,8 +944,9 @@ class ClusterHandler(
   /**
    * Transforms CloudDriver response to our server group model.
    */
-  private fun ActiveServerGroup.toServerGroup() =
-    ServerGroup(
+  private fun ActiveServerGroup.toServerGroup() : ServerGroup {
+    val launchTemplateData = launchTemplate?.launchTemplateData
+    return ServerGroup(
       name = name,
       location = Location(
         account = accountName,
@@ -954,19 +955,18 @@ class ClusterHandler(
         subnet = subnet(cloudDriverCache),
         availabilityZones = zones
       ),
-      launchConfiguration = launchConfig.run {
+      launchConfiguration =
         LaunchConfiguration(
-          imageId = imageId,
+          imageId = launchConfig?.imageId ?: launchTemplateData!!.imageId,
           appVersion = image.appVersion,
           baseImageVersion = image.baseImageVersion,
-          instanceType = instanceType,
-          ebsOptimized = ebsOptimized,
-          iamRole = iamInstanceProfile,
-          keyPair = keyName,
-          instanceMonitoring = instanceMonitoring.enabled,
-          ramdiskId = ramdiskId.orNull()
-        )
-      },
+          instanceType = launchConfig?.instanceType ?: launchTemplateData!!.instanceType,
+          ebsOptimized = launchConfig?.ebsOptimized ?: launchTemplateData!!.ebsOptimized,
+          iamRole = launchConfig?.iamInstanceProfile ?: launchTemplateData!!.iamInstanceProfile.name,
+          keyPair = launchConfig?.keyName ?: launchTemplateData!!.keyName,
+          instanceMonitoring = launchConfig?.instanceMonitoring?.enabled ?: launchTemplateData!!.monitoring.enabled,
+          ramdiskId = launchConfig?.ramdiskId.orNull() // not in launchTemplateData
+        ),
       buildInfo = buildInfo?.toEc2Api(),
       capacity = capacity.let {
         when (scalingPolicies.isEmpty()) {
@@ -995,6 +995,7 @@ class ClusterHandler(
       image = image.toEc2Api(),
       instanceCounts = instanceCounts.toEc2Api()
     )
+  }
 
   private fun List<MetricDimensionModel>?.toSpec(): Set<MetricDimension> =
     when (this) {
