d64fe3420d786b34a32d597e726d01e8151add29
Rob Fletcher <rfletcher@netflix.com>
2020-12-15 10:45:04 -0800

    feat(verification): fix subject, description and correlation id of job

--- keel-titus-plugin/src/main/kotlin/com/netflix/spinnaker/keel/titus/verification/ContainerTestVerificationEvaluator.kt
+++ keel-titus-plugin/src/main/kotlin/com/netflix/spinnaker/keel/titus/verification/ContainerTestVerificationEvaluator.kt
@@ -59,9 +59,9 @@ class ContainerTestVerificationEvaluator(
     return runBlocking {
       withContext(IO) {
         taskLauncher.submitJob(
-          subject = "container integration test",
-          description = "Verifying environment ${context.environmentName} with test container ${verification.container}",
-          correlationId = "keel-run-container-test",
+          subject = "container integration test for ${context.deliveryConfig.application}.${context.environmentName}",
+          description = "Verifying environment ${context.environmentName} with test container ${verification.repository}/${verification.tag}",
+          correlationId = "${supportedVerification.first}:${context.deliveryConfig.application}.${context.environmentName}",
           user = context.deliveryConfig.serviceAccount,
           application = context.deliveryConfig.application,
           notifications = emptySet(),
