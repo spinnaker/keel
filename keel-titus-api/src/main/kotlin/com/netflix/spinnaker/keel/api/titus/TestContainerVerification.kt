87373558bf8634053d5ac787d255a6df786780c8
Rob Fletcher <rfletcher@netflix.com>
2021-01-04 14:43:39 -0800

    fix(verification): allow specification of application for test container cluster

--- keel-titus-api/src/main/kotlin/com/netflix/spinnaker/keel/api/titus/TestContainerVerification.kt
+++ keel-titus-api/src/main/kotlin/com/netflix/spinnaker/keel/api/titus/TestContainerVerification.kt
@@ -6,7 +6,8 @@ import com.netflix.spinnaker.keel.api.titus.TitusServerGroup.Location
 data class TestContainerVerification(
   val repository: String,
   val tag: String = "latest",
-  val location: Location
+  val location: Location,
+  val application: String? = null
 ) : Verification {
   override val type = TYPE
   override val id = "$repository/$tag"
